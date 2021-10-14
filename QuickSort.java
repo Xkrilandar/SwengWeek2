6f8f8bd28c624e5db57489ca66938f0af60c3510
Xavier Theimer-Lienhard <xavier.theimer-lienhard@epfl>
2021-10-14 13:54:27 +0200

    Fix sorting algorithm

--- QuickSort.java
+++ QuickSort.java
@@ -20,8 +20,7 @@ class QuickSort{
         return i+1;
     }
     void sort(int arr[], int low, int high) {
-        //TODO the > sign should be swapped with <
-        if (low > high){
+        if (low < high){
             int pi = partition(arr, low, high);
 
             sort(arr, low, pi-1);
